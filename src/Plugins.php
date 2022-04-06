4617188cbe15ad4c3d696e4ee6a4309f0f44655a
Brandon Kramer <hello@brandonkramer.cc>
2022-04-06 10:06:48 +0200

    0.3.2: Add Plugins Utility

---
+++ src/Plugins.php
@@ -0,0 +1,17 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Pangolia\Utils;
+
+class Plugins {
+
+	/**
+	 * Determines if the plugin is active
+	 *
+	 * @param string $plugin
+	 * @return bool
+	 */
+	public static function is_active( string $plugin ): bool {
+		return \in_array( $plugin, (array) \get_option( 'active_plugins', array() ) );
+	}
+}
